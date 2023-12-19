3434ea8ed259074840e2c753cd7f16601d6adabb
arjunmnath <arjunmanjunath80@gmail.com>
2023-12-19 16:26:32 +0530

    Note: UI basics completion

---
+++ app/login/page.tsx
@@ -0,0 +1,106 @@
+import Image from "next/image";
+import pic from "@/public/thumbnail.png";
+import google from "@/public/google.svg";
+import vector from "@/public/vector.svg";
+import Link from "next/link";
+
+const Login = () => {
+  return (
+    <>
+      <div className="flex flex-row text-[#525252]">
+        <div className="portrait:hidden w-[60vw] object-cover">
+            <img src="/thumbnail.png" alt="alternate" />
+        </div>
+        <div className="actions w-[40vw] portrait:w-[100vw] h-screen">
+          {/* frame-14 */}
+          <div className="m-16">
+            <Image className="my-4" src={vector} alt="" />
+            {/* frame-13 */}
+            <div className="flex flex-col gap-[2rem]">
+              <div className="frame-1">
+                {/* 36px bold */}
+                <h2 className="text-[2.25rem] font-bold ">
+                  Login to your Account
+                </h2>
+
+                {/* 16px */}
+                <h3 className="text-base text-[#525252]">
+                  See what is going on with your business
+                </h3>
+              </div>
+              <button className="frame-1 flex w-full justify-center py-1 border border-solid border-[#E8E8E8]  rounded-[.3rem] flex-row text-[#828282]">
+                <Image className="mx-2" src={google} alt="" />
+                Continue with Google
+              </button>
+            </div>
+            <div className="relative py-[1.5rem]">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t border-[#a1a1a1]"></span>
+              </div>
+              <div className="relative flex justify-center">
+                <span className="bg-white px-2 text-[#a1a1a1]">
+                  or Sign in with Email
+                </span>
+              </div>
+            </div>
+
+            {/* frame-12 */}
+            <div className="frame-12">
+              {/* frame-11 */}
+              <div className="frame-11 flex gap-7 flex-col">
+                {/* frame-5 */}
+                <div className="frame-5 ">
+                  {/* 14px */}
+                  <h4 className="text-[#828282]">Email</h4>
+                  <input
+                    className="border border-solid border-[#e8e8e8] w-full rounded-[.3rem] p-2 placeholder:text-[#e0e0e0]"
+                    type="text"
+                    placeholder="mail@abc.com"
+                  />
+                </div>
+                <div className="frame-9 ">
+                  <div className="frame-6">
+                    {/* 14px */}
+                    <h4 className="text-[#828282]">Password</h4>
+                    <input
+                      className="border border-solid border-[#e8e8e8] w-full rounded-[.3rem] p-2 placeholder:text-[#e0e0e0] placeholder:align-middle focus:border focus:border-solid focus:border-red-800"
+                      type="password"
+                      placeholder="••••••••••••"
+                    />
+                  </div>
+                  <div className="frame-8 mt-[0.625rem] flex flex-row justify-between">
+                    <div className="remember items-center flex">
+                      <input
+                        className="mx-1 h-[1em] w-[1em] accent-[#7F265B]"
+                        type="checkbox"
+                        name="rme"
+                        id="rme"
+                      />
+                      <label className="text-[#a1a1a1]" htmlFor="rme">
+                        Remember Me{" "}
+                      </label>
+                    </div>
+                    <Link href="#" className="text-[#7F265B] font-semibold">
+                      Forgot Password?
+                    </Link>
+                  </div>
+                </div>
+              </div>
+              <button className="frame-7 mt-[2rem] bg-[#7F265B] text-white text-center w-full font-bold py-3 rounded-md">
+                Login
+              </button>
+            </div>
+          </div>
+          <p className="text-center relative top-1">
+            Not Registered Yet?{" "}
+            <Link href="#" className="text-[#7F265B] font-semibold">
+              Create an account
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,46 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    theme: {
+      screens: {
+        'sm': {'max': '767px'},
+        // => @media (min-width: 640px) { ... }
+  
+        'md': {'min': '767px', 'max': '991px'},
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': {'min': '1200px', 'max': '1439px'},
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': {'min': '1440px'},
+        // => @media (min-width: 1280px) { ... }
+  
+        // '2xl': '1536px',
+        // // => @media (min-width: 1536px) { ... }
+      }
+    }
   },
   plugins: [],
 }
 export default config
+
+
+/* For mobile phones 
+@media only screen and (max-width: 767px) {
+  CSS styles specific to mobile phones 
+}
+
+ For tablets 
+@media only screen and (min-width: 768px) and (max-width: 991px) {
+   CSS styles specific to tablets 
+}
+
+For small laptops and large tablets 
+@media only screen and (min-width: 1200px) and (max-width: 1439px) {
+   CSS styles specific to small laptops and large tablets 
+}
+
+For medium to large laptops and desktops 
+@media only screen and (min-width: 1440px) {
+   CSS styles specific to medium to large laptops and desktops 
+}*/
